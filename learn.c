cf1d9875e2ca837b62700c2ecc99650204299312
Eduardo Metring <67509355+eduardometring@users.noreply.github.com>
2020-10-08 08:21:08 -0300

    bruh

--- learn.c
+++ learn.c
@@ -3,5 +3,11 @@ int main() {
   int gay = 4;
   printf("%d", gay);
   printf("\nJaime Automóveis is gay\n");
+  int age = 62;
+  printf("Jaime Automóveis is %d", age);
+  printf("\n");
+  age = 13;
+  printf("Pedrinho Gameplays is %d", age);
+  printf("\n");
   return 0;
 }
